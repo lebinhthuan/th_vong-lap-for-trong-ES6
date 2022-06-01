1721b54d682cd815eaa7571700f3d85d61f43547
lebinhthuan <104605059+lebinhthuan@users.noreply.github.com>
2022-06-01 17:27:04 +0700

    update - Buoc 3

--- for-loop-example.js
+++ for-loop-example.js
@@ -1,4 +1,6 @@
-console.log("for...of")
-for (let i of ['Ha Noi', 20.32, 3000]) {
-    console.log(i + " ")
+let obj = {name: 'Peter', age: 25, salary: 3000};
+
+console.log("for...in")
+for (let i in obj) {
+    console.log(obj[i] + " ");
 }
