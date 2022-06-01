d9dad6a3c4c5ec231e67f3fdc21d856e5c4e8084
lebinhthuan <104605059+lebinhthuan@users.noreply.github.com>
2022-06-01 17:25:55 +0700

    update - Buoc 2

--- for-loop-example.js
+++ for-loop-example.js
@@ -1,7 +1,4 @@
-function geeks() {
-    console.log("for(;;)")
-    for (let i = 0; i <= 10; i += 2) {
-        console.log(i + " ")
-    }
+console.log("for...of")
+for (let i of ['Ha Noi', 20.32, 3000]) {
+    console.log(i + " ")
 }
-geeks();
